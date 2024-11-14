89725ad7da2572457b6a8fa6df61d9f30eb25884
madarasibalazs <23IMC10623@fh-krems.ac.at>
2024-11-14 11:52:28 +0100

    Completed subtasks 1-3

--- assignment.c
+++ assignment.c
@@ -11,8 +11,55 @@ int main(int argc, char *argv[]) {
     int minrand = 1;
     int maxrand = 100;
 
-    // WRITE YOUR CODE HERE
+    // Subtask 1: checking the number of arguments
+    if (argc != 3) {  // checking if argc does not equal 3 --> I have to check it with 3 since argc includes the program name as the first argument 
+        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2\n", argc-1); // The -1 is needed because argc includes the program name as the first argument 
+        return 0;
+    }
     
+    // Subtask 2: checking whether the arguments are positive integers or not
+    for (int i = 1; i < argc; i++) {
+        char *end;
+        long val = strtol(argv[i], &end, 10); // Convert the argument to a long integer
+
+        // Check if conversion is successful and if value is greater than 0
+        if (*end != '\0' || val <= 0) {
+            printf("Incorrect usage. The parameters you provided are not positive integers\n");
+            return 0;
+        }
+    }
+
+    // Subtask 3: creating the matrix based on the arguments
+    int nrows = (int)strtol(argv[1], NULL, 10);  // Number of rows
+    int ncols = (int)strtol(argv[2], NULL, 10);  // Number of columns
+
+    // Memory allocation
+    int **theArray = (int**) malloc(nrows * sizeof(int*));
+    for (int i = 0; i < nrows; i++) {
+        theArray[i] = (int*) malloc(ncols*sizeof(int));
+    }
+
+    // Fill the matrix with random values between 1 and 100
+    for(int i = 0; i < nrows; ++i) {
+        for (int j = 0; j < ncols; j++) {
+            theArray[i][j] = minrand + rand() % (maxrand - minrand + 1);
+        }
+    }
+
+    // Print the matrix to the console (this is just for myself, solely for testing purposes)
+    for (int i = 0; i < nrows; i++) {
+        for (int j = 0; j < ncols; j++) {
+            printf("%d\t", theArray[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Free allocated memory (free each row and then the array of row pointers)
+    for (int i = 0; i < nrows; i++) {
+        free(theArray[i]);
+    }
+    free(theArray);
+
 
     return 0;
 }
